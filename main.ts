084ee68a78a681b8084dc3918b500352eaae43fc
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-22 19:01:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    Number_1 += 1
+    basic.showNumber(Number_1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    Number_1 += -1
+    basic.showNumber(Number_1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Number_1)
+    basic.showString("+")
+    basic.showNumber(Number_2)
+    basic.showString("=")
+    basic.showNumber(Number_1 + Number_2)
+})
+input.onButtonPressed(Button.B, function () {
+    Number_2 += 1
+    basic.showNumber(Number_2)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(Number_1)
+    basic.showString("-")
+    basic.showNumber(Number_2)
+    basic.showString("=")
+    basic.showNumber(Number_1 - Number_2)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    Number_2 += -1
+    basic.showNumber(Number_2)
+})
+let Number_2 = 0
+let Number_1 = 0
+Number_1 = 0
+Number_2 = 0
