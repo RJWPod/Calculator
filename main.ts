59f585a11f3a8de6922ad5e87ea5876cdd417f4b
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-23 09:52:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,50 @@
+// increase value
+// 
 input.onButtonPressed(Button.A, function () {
     Number_1 += 1
     basic.showNumber(Number_1)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(Number_1)
-    basic.showString("+")
-    basic.showNumber(Number_2)
-    basic.showString("=")
-    basic.showNumber(Number_1 + Number_2)
+    if (Number_2 == 0) {
+        basic.showString("+")
+        Number_2 = Number_1
+        Number_1 = 0
+    }
+    if (Number_1 != 0) {
+        basic.showString("=")
+        basic.showNumber(Number_1 + Number_2)
+    }
+    if (Number_1 == 0) {
+        basic.showNumber(Number_1)
+        basic.showString("=")
+        basic.showNumber(Number_1 + Number_2)
+    }
 })
+// decrease value
+// 
 input.onButtonPressed(Button.B, function () {
     Number_1 += -1
     basic.showNumber(Number_1)
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(Number_1)
-    basic.showString("-")
-    basic.showNumber(Number_2)
-    basic.showString("=")
-    basic.showNumber(Number_1 - Number_2)
+    if (Number_2 == 0) {
+        basic.showString("-")
+        Number_2 = Number_1
+        Number_1 = 0
+    }
+    if (Number_1 != 0) {
+        basic.showString("=")
+        basic.showNumber(Number_2 - Number_1)
+    }
+    if (Number_1 == 0) {
+        basic.showNumber(Number_1)
+        basic.showString("=")
+        basic.showNumber(Number_2 - Number_1)
+    }
 })
+// show image initalize #1 #2
 let Number_2 = 0
 let Number_1 = 0
+basic.showIcon(IconNames.TShirt)
 Number_1 = 0
 Number_2 = 0
